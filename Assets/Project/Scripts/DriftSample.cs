7e1bf93acddcba84241f9f196d1b0822e27c49ee
edom18 <edo.m18@gmail.com>
2025-06-21 09:25:41 +0900

    feat: ブースト・ブレーキの可視化用UIを追加

--- Assets/Project/Scripts/DriftSample.cs
+++ Assets/Project/Scripts/DriftSample.cs
@@ -17,8 +17,8 @@ public class DriftSample : MonoBehaviour
     ///
     /// よって、v_terget = a / k
     /// km/s^2 / s^-1 = km/s として、単位が一致
-    
     [SerializeField] private float _targetSpeed = 10f;
+
     [SerializeField] private float _acc = 2f;
     [SerializeField] private float _rotateSpeed = 1f;
     [SerializeField] private float _turnRotateSpeed = 1.0f;
@@ -26,13 +26,15 @@ public class DriftSample : MonoBehaviour
     [SerializeField] private float _dragScale = 2f;
     [SerializeField] private KeyCode _burstKey = KeyCode.W;
     [SerializeField] private KeyCode _breakKey = KeyCode.Space;
-    
+
     [SerializeField] private TMP_Text _velocityText;
-    
+    [SerializeField] private TMP_Text _burstText;
+    [SerializeField] private TMP_Text _breakText;
+
     private float Drag => _acc / _targetSpeed;
 
     private Vector3 _velocoty = Vector3.zero;
-    
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.A))
@@ -43,27 +45,39 @@ public class DriftSample : MonoBehaviour
         {
             transform.Rotate(Vector3.up, _rotateSpeed, Space.Self);
         }
-        
+
         Vector3 forward = transform.forward;
 
         float acceleration = _acc;
         if (Input.GetKey(_burstKey))
         {
             acceleration *= _burstScale;
+
+            _burstText.text = "on";
+        }
+        else
+        {
+            _burstText.text = "off";
         }
 
         float drag = Drag;
         if (Input.GetKey(_breakKey))
         {
             drag *= _dragScale;
+
+            _breakText.text = "on";
         }
-        
+        else
+        {
+            _breakText.text = "off";
+        }
+
         _velocoty += forward * (acceleration * Time.deltaTime);
         _velocoty = Vector3.RotateTowards(_velocoty, forward * _velocoty.magnitude, _rotateSpeed * Time.deltaTime, 0);
-        
+
         _velocoty -= _velocoty * (drag * Time.deltaTime);
         transform.position += _velocoty * Time.deltaTime;
-        
+
         _velocityText.text = $"{_velocoty.magnitude:F2} km/s";
     }
 }
